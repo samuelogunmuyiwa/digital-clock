b9a9821417db2bebd441342cb1a5248dd3a3e5af
Larry-cool <larrycoolboy92@gmail.com>
2023-11-21 06:12:58 +0100

    wrote the Js code including setInterval

---
+++ script.js
@@ -0,0 +1,16 @@
+let hours = document.getElementById('hours'); 
+let min = document.getElementById('min'); 
+let sec = document.getElementById('sec'); 
+
+setInterval(()=>{
+
+	let currentTime = new Date();
+
+console.log(currentTime);
+
+hours.innerHTML = (currentTime.getHours()<10?"0":"") + currentTime.getHours();
+min.innerHTML = (currentTime.getMinutes()<10?"0":"") + currentTime.getMinutes();
+sec.innerHTML = (currentTime.getSeconds()<10?"0":"") + currentTime.getSeconds();
+
+},1000)
+
